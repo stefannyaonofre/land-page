e294c7c4a728a561bc8618430eb79000c901cbe6
Stefannya <stefannyaonofre@gmail.com>
2023-07-07 19:02:34 -0500

    creation responsive

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -1,29 +1,57 @@
 import "../styles/style.scss";
 
-
-document.addEventListener('DOMContentLoaded', () => {
-
-});
-
+const menuContainer = document.querySelector('.menu__button');
+const menuMovile = document.querySelector('.menu');
+const moreFeacture = document.getElementById('more__feacture');
+const lessFeacture = document.getElementById('less__feacture');
+const menuFeacture = document.getElementById('menu__feacture');
+const moreCompany = document.getElementById('more__company');
+const lessCompany = document.getElementById('less__company');
+const menuCompa = document.getElementById('menu__compa');
 const menuFeatures = document.getElementById('expand_less');
 const menuFeatur = document.getElementById('expand_more');
 const menuMostrar = document.getElementById('dropdown-menu');
-
 const expandMore = document.getElementById('expand_moreCompany');
 const expandLess = document.getElementById('expand_lessCompany');
 const menuCompany = document.getElementById('dropdown-menuCompany');
 
+moreFeacture.addEventListener('click', (e) => {
+    e.preventDefault();
+    moreFeacture.classList.add('hidden');
+    lessFeacture.classList.remove('hidden');
+    menuFeacture.classList.remove('hidden');
+});
+
+lessFeacture.addEventListener('click', (e) => {
+    e.preventDefault();
+    moreFeacture.classList.remove('hidden');
+    lessFeacture.classList.add('hidden');
+    menuFeacture.classList.add('hidden');
+});
+
+moreCompany.addEventListener('click', (e) => {
+    e.preventDefault();
+    moreCompany.classList.add('hidden');
+    lessCompany.classList.remove('hidden');
+    menuCompa.classList.remove('hidden');
+});
+
+lessCompany.addEventListener('click', (e) => {
+    e.preventDefault();
+    moreCompany.classList.remove('hidden');
+    lessCompany.classList.add('hidden');
+    menuCompa.classList.add('hidden');
+});
+
 menuFeatur.addEventListener('click', (e) => {
     e.preventDefault();
-
-    menuFeatur.classList.add('hidden'); //oculta
-    menuMostrar.classList.remove('hidden');//se le quita la clase hidden al menu que esta oculto
-    menuFeatures.classList.remove('hidden');//para ocultar la flecha hacia arriba
+    menuFeatur.classList.add('hidden');
+    menuMostrar.classList.remove('hidden');
+    menuFeatures.classList.remove('hidden');
 });
 
 menuFeatures.addEventListener('click', (e) => {
     e.preventDefault();
-
     menuFeatur.classList.remove('hidden'); 
     menuMostrar.classList.add('hidden');
     menuFeatures.classList.add('hidden');
@@ -31,7 +59,6 @@ menuFeatures.addEventListener('click', (e) => {
 
 expandMore.addEventListener('click', (e) => {
     e.preventDefault();
-
     expandMore.classList.add('hidden'); 
     expandLess.classList.remove('hidden');
     menuCompany.classList.remove('hidden');
@@ -39,8 +66,11 @@ expandMore.addEventListener('click', (e) => {
 
 expandLess.addEventListener('click', (e) => {
     e.preventDefault();
-
     expandMore.classList.remove('hidden'); 
     expandLess.classList.add('hidden');
     menuCompany.classList.add('hidden');
 });
+
+menuContainer.addEventListener('click', () => {
+    menuMovile.classList.add('active');
+})
